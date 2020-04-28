e5490fa6ce7be3834f33c9ddbb068e762ae80ce9
dangrammer <danieljromans@gmail.com>
2020-04-28 16:49:02 -0400

    added user profile edit validations for bio and credits length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :username, :first_name, :last_name, :image_url, :city, :state, :zip_code, presence: true, on: [:create, :update]
+  validates :username, :first_name, :last_name, :image_url, :city, :state, :zip_code, presence: true
   validates :password, length: {in: 5..15}, on: :create
   validates :username, uniqueness: true, length: {in: 2..15}
   validates :first_name, :last_name, :city, :state, :zip_code, length: {in: 2..20}
+  validates :bio, :credits, length: {maximum: 2000}
 
   has_many :listings, dependent: :destroy
   has_many :applications, :foreign_key => 'applicant_id', dependent: :destroy
