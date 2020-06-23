d98b2a6960fa961148f78c005c2e94e94ae1166b
dangrammer <danieljromans@gmail.com>
2020-06-23 16:03:38 -0400

    edited subscribe methods for conversations and messages channels

--- app/channels/conversations_channel.rb
+++ app/channels/conversations_channel.rb
@@ -1,6 +1,6 @@
 class ConversationsChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "current_user_#{current_user.id}" #application_cable needs to be edited to accomodate this
   end
 
   def unsubscribed
--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,6 +1,7 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    conversation = Conversation.find(params[:conversation])
+    stream_for conversation
   end
 
   def unsubscribed
