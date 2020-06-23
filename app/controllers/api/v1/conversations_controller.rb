306b90abff43c1ab499bd82bee869d255386bd14
dangrammer <danieljromans@gmail.com>
2020-06-23 16:33:19 -0400

    added create action to messages controller

--- app/controllers/api/v1/conversations_controller.rb
+++ app/controllers/api/v1/conversations_controller.rb
@@ -6,14 +6,13 @@ class Api::V1::ConversationsController < ApplicationController
   def create
     conversation = Conversation.new(conversation_params)
     if conversation.save
-
       # serialized_data = ActiveModelSerializers::Adapter::Json.new(
       #   ConversationSerializer.new(conversation)
       # ).serializable_hash
 
       # testing this code for FastJSON API needs
       serialized_data = CategorySerializer.new(conversation)
-      
+
       ActionCable.server.broadcast 'conversations_channel', serialized_data
       head :ok
     end
--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::MessagesController < ApplicationController
+  def create
+    message = Message.new(message_params)
+    conversation = Conversation.find(message_params[:conversation_id])
+    
+    if message.save
+      # serialized_data = ActiveModelSerializers::Adapter::Json.new(
+      #   MessageSerializer.new(message)
+      # ).serializable_hash
+
+      # testing this code for FastJSON API needs
+      serialized_data = MessageSerializer.new(message)
+
+      MessagesChannel.broadcast_to conversation, serialized_data
+      head :ok
+    end
+  end
+  
+  private
+  
+  def message_params
+    params.require(:message).permit(:text, :conversation_id, :user_id)
+  end
 end
