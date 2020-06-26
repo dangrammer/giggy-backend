e016f6d8c3b05171919ffece6f6142847cce922f
dangrammer <danieljromans@gmail.com>
2020-06-26 17:29:23 -0400

    altered messages controller to use .create and .valid? instead of .new and .save

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::MessagesController < ApplicationController
   def create
-    message = Message.new(message_params)
+    message = Message.create(message_params)
     conversation = Conversation.find(message_params[:conversation_id])
-    
-    if message.save
+
+    if message.valid?
       # serialized_data = ActiveModelSerializers::Adapter::Json.new(
       #   MessageSerializer.new(message)
       # ).serializable_hash
