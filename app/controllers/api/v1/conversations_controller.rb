2a3b29c8e5aadcf7556b621331b32dc23609805a
dangrammer <danieljromans@gmail.com>
2020-07-09 17:00:03 -0400

    tried changing broadcast syntax in convo controller

--- app/controllers/api/v1/conversations_controller.rb
+++ app/controllers/api/v1/conversations_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::ConversationsController < ApplicationController
   end
 
   def create
+    # byebug
     conversation = Conversation.create()
    
     if conversation.valid?
@@ -14,11 +15,14 @@ class Api::V1::ConversationsController < ApplicationController
       UserConversation.create(
         conversation_id: conversation.id, 
         user_id: conversation_params[:receiver_id]
-        )
+      )
+
       serialized_data = ConversationSerializer.new(conversation)
       
-      ActionCable.server.broadcast "current_user_#{conversation_params[:sender_id]}", serialized_data
-      ActionCable.server.broadcast "current_user_#{conversation_params[:receiver_id]}", serialized_data
+      # ActionCable.server.broadcast "current_user_#{conversation_params[:sender_id]}", serialized_data
+      # ActionCable.server.broadcast "current_user_#{conversation_params[:receiver_id]}", serialized_data
+      ConversationsChannel.broadcast_to("current_user_#{conversation_params[:sender_id]}", serialized_data)
+      ConversationsChannel.broadcast_to("current_user_#{conversation_params[:receiver_id]}", serialized_data)
       head :ok
     end
   end
@@ -26,7 +30,7 @@ class Api::V1::ConversationsController < ApplicationController
   private
   
   def conversation_params
-    params.require(:conversation).permit(:sender_id, :receiver_id)
+    params.permit(:conversation, :sender_id, :receiver_id) #.require(:conversation)
   end
 
 end
